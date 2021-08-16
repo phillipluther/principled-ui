fc0f38f49a89c2a172ccb901c127aaced2c347f9
Phillip Luther <dev@phillipluther.com>
2021-08-16 07:24:15 -0700

    Update VisuallyHidden component

--- src/components/a11y/visually-hidden/visually-hidden.stories.tsx
+++ src/components/a11y/visually-hidden/visually-hidden.stories.tsx
@@ -8,11 +8,10 @@ export default {
   component: VisuallyHidden,
 } as Meta;
 
-const Template: Story<VisuallyHiddenProps> = (args) => <VisuallyHidden {...args} />;
+export const Default: Story<VisuallyHiddenProps> = () => (
+  <VisuallyHidden>This text is visually hidden.</VisuallyHidden>
+);
 
-export const Default = Template.bind({});
-export const CustomTag = Template.bind({});
-
-CustomTag.args = {
-  as: 'h2',
-};
+export const CustomTag: Story<VisuallyHiddenProps> = () => (
+  <VisuallyHidden as="h2">This H2 is visually hidden.</VisuallyHidden>
+);
--- src/components/a11y/visually-hidden/visually-hidden.tsx
+++ src/components/a11y/visually-hidden/visually-hidden.tsx
@@ -16,4 +16,15 @@ const VisuallyHidden: React.FC<VisuallyHiddenProps> = ({
   >{children}</TagName>
 );
 
-export default VisuallyHidden;
+export default styled(VisuallyHidden)`
+  border: 0;
+  clip: rect(1px, 1px, 1px, 1px);
+  clip-path: inset(50%);
+  height: 1px;
+  margin: -1px;
+  overflow: hidden;
+  padding: 0;
+  position: absolute;
+  width: 1px;
+  word-wrap: normal !important;
+`;
